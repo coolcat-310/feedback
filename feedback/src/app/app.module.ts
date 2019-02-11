470b4568aa78e4677f2a8957571c8e9545d984ba
Juan Navarrete <navarretejuan09@gmail.com>
2019-02-10 19:00:44 -0800

    add several components and router

--- feedback/src/app/app.module.ts
+++ feedback/src/app/app.module.ts
@@ -1,17 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { QuestionsComponent } from './questions/questions.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
+
+const routes: Routes = [
+  // basic routes
+  {path: '', component: HomeComponent},
+  {path: 'question/:username', component: QuestionsComponent},
+  {path: 'employee', component: EmployeeComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionsComponent
+    QuestionsComponent,
+    EmployeeComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      routes,
+    ),
     BrowserModule
   ],
   providers: [],
